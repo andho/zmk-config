101d955aa2f858d0643376994b264b6f38180fce
Amjad Mohamed <andhos@gmail.com>
2022-01-21 16:24:10 +0500

    Seems good enough

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+podman run --rm -it -v $(pwd):/app -w /app -v zmk-home:/root zmkfirmware/zephyr-west-action-arm build "-p"
+podman run --rm -it -v $(pwd):/app -w /app -v zmk-home:/root zmkfirmware/zephyr-west-action-arm build "-s zmk/app -b nice_nano_v2 -- -DZMK_CONFIG=/app/config -DSHIELD=kyria_left"
+cp build/zephyr/zmk.uf2 ../kyria_left-nice_nano_v2-zmk.uf2
+
+podman run --rm -it -v $(pwd):/app -w /app -v zmk-home:/root zmkfirmware/zephyr-west-action-arm build "-p"
+podman run --rm -it -v $(pwd):/app -w /app -v zmk-home:/root zmkfirmware/zephyr-west-action-arm build "-s zmk/app -b nice_nano_v2 -- -DZMK_CONFIG=/app/config -DSHIELD=kyria_right"
+cp build/zephyr/zmk.uf2 ../kyria_right-nice_nano_v2-zmk.uf2
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+podman pull zmkfirmware/zephyr-west-action-arm
+
